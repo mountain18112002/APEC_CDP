40e4a36df18ed259baf5b31414c69b659e084924
trongquan02 <vuquan.13122002@gmail.com>
2024-01-22 14:21:43 +0700

    set time

--- updata/main.py
+++ updata/main.py
@@ -4,8 +4,7 @@ import schedule
 import time
 
 if __name__ == "__main__":
-    upload_all_dataframes()
-    # schedule.every().day.at("08:00").do(upload_all_dataframes)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
+    schedule.every().day.at("08:00").do(upload_all_dataframes)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
